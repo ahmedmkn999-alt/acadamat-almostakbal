26fb8eeebdc592109e8e1ca5497911e8c34c87a7
ahmedmkn999-alt <ahmedmkn999@gmail.com>
2025-12-28 06:41:27 +0200

    Create data_science.js

---
+++ data_science.js
@@ -0,0 +1,85 @@
+// اسم الملف: data_science.js
+
+const SCIENCE_RAW = 
+
+// افتح الرابط ده: https://plus-teal.vercel.app/organized_output.json
+// انسخ كل الكلام اللي جواه وحطه مكان الكلمة اللي تحت دي
+
+("[
+  {
+    "id": 3,
+    "name": "2026",
+    "image_url": "https://files.coursatk.online/images/content/20251213_7f3c5a5d26b34ddb709f1c653533d299.jpg",
+    "created_at": "2025-10-02T16:23:31.615202Z",
+    "updated_at": "2025-12-13T16:02:06.19909Z",
+    "subjects": [
+      {
+        "id": 40,
+        "name": "لغة عربية",
+        "image_url": "https://i.ibb.co/P9DxZ48/photo-2023-07-20-23-53-0.jpg",
+        "order_index": 1,
+        "is_published": true,
+        "published_at": "2025-11-16T17:21:40.814375Z",
+        "year_id": 3,
+        "created_at": "2025-11-16T17:21:40.806523Z",
+        "updated_at": "2025-11-16T17:21:40.806523Z",
+        "teachers": [
+          {
+            "id": 109,
+            "name": "أ. رضا الفاروق",
+            "image_url": "https://i.ibb.co/jkwYwHNJ/mrr.jpg",
+            "chapter_count": 2,
+            "chapters": [
+              {
+                "id": 524,
+                "name": "الترم الأول",
+                "image_url": "https://i.ibb.co/KdKCJq0/1stterm.jpg",
+                "teacher_name": "أ. رضا الفاروق",
+                "teacher_image_url": "https://i.ibb.co/jkwYwHNJ/mrr.jpg",
+                "subject_name": "لغة عربية",
+                "subject_image_url": "https://i.ibb.co/P9DxZ48/photo-2023-07-20-23-53-0.jpg",
+                "lecture_count": 16,
+                "lectures": [
+                  {
+                    "id": 3927,
+                    "name": "المحاضرة 15",
+                    "image_url": "https://files.coursatk.online/images/content/20251218_a74767ee9cb68faf0ac21cc6b3cf7939.jpg",
+                    "chapter_name": "الترم الأول",
+                    "chapter_image_url": "https://i.ibb.co/KdKCJq0/1stterm.jpg",
+                    "teacher_name": "أ. رضا الفاروق",
+                    "teacher_image_url": "https://i.ibb.co/jkwYwHNJ/mrr.jpg",
+                    "subject_name": "لغة عربية",
+                    "subject_image_url": "https://i.ibb.co/P9DxZ48/photo-2023-07-20-23-53-0.jpg",
+                    "video_count": 14,
+                    "pdf_count": 0,
+                    "exam_count": 1,
+                    "videos": [
+                      {
+                        "id": 14703,
+                        "title": "محتوى المحاضرة الـ 15",
+                        "stream_url": "https://river-3-344.rtbcdn.ru/hls-vod/HgPMvgvaabK5kW5uFdYKpA/1767200235/3320/0x5000c500e9ee263c/831db01913874978980d98501b896df8.mp4.m3u8?i=640x360_457"
+                      },
+                      {
+                        "id": 14704,
+                        "title": "الاسم المنقوص",
+                        "stream_url": "https://river-3-314.rtbcdn.ru/hls-vod/MQb7oDpdTZWkKYSPt3ERUg/1767200242/2692/0x5000039ce8694319/db9c41ade55943b1839c2942bc977c1a.mp4.m3u8?i=640x360_340"
+                      },
+                      {
+                        "id": 14705,
+                        "title": "الاسم الممدود",
+                        "stream_url": "https://river-5-546.rtbcdn.ru/hls-vod/44BQ8hnr_ZGWIe0xo68csw/1766951252/3282/0x5000c500fa77633b/64dcfd92bb8c4afe98ff641e430b3337.mp4.m3u8?i=640x360_347"
+                      },
+                      {
+                        "id": 14706,
+                        "title": "يلا نحل سوا على الاسم المنقوص والممدود",
+                        "stream_url": "https://river-3-316.rtbcdn.ru/hls-vod/GBH6Sb6x69gk9lMi82Ozqg/1766942886/3268/0x5000c500dc7ccede/f95596daffaf459293d51bd9c57070ad.mp4.m3u8?i=640x360_402"
+                      },
+                      {
+                        "id": 14707,
+                        "title": "ملخص بناء الفعل للمجهول",
+                        "stream_url": "https://river-5-538.rtbcdn.ru/hls-vod/mnc3Z9Nw3XcoRrBU1uIipA/1766953168/3344/0x5000c500e9cf76a4/dcd132d939744635af0ca4acb187a8ab.mp4.m3u8?i=640x360_435"
+                      },
+                      {
+                        "i)
+
+;
