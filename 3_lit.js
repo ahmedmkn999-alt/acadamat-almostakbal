46c538554175d6b440cf66cd8ae58b097be61e21
ahmedmkn999-alt <ahmedmkn999@gmail.com>
2025-12-28 05:59:55 +0200

    Create 3_lit.js

---
+++ 3_lit.js
@@ -0,0 +1,40 @@
+const LIT_DATA = {
+    "التاريخ": {
+        "مستر إسلام عزيز": [
+            { name: "الحصة 1: الحملة الفرنسية على مصر", link: "https://www.youtube.com/watch?v=video_id_hist1" },
+            { name: "الحصة 2: نزول الحملة والمقاومة", link: "https://www.youtube.com/watch?v=video_id_hist2" },
+            { name: "الحصة 3: جلاء الحملة الفرنسية", link: "https://www.youtube.com/watch?v=video_id_hist3" }
+        ]
+    },
+    "الجغرافيا": {
+        "مستر جمعة السيد": [
+            { name: "الدرس التمهيدي: الجغرافيا السياسية", link: "https://www.youtube.com/watch?v=video_id_geo1" },
+            { name: "الدولة تعريفها وأنواعها", link: "https://www.youtube.com/watch?v=video_id_geo2" }
+        ]
+    },
+    "علم النفس والاجتماع": {
+        "مستر نادر جورج": [
+            { name: "الذكاء الواحد والذكاءات المتعددة", link: "https://www.youtube.com/watch?v=video_id_psy1" },
+            { name: "نظريات التعلم", link: "https://www.youtube.com/watch?v=video_id_psy2" }
+        ],
+         "مستر تامر صفوت": [
+             { name: "مبادئ علم النفس", link: "https://www.youtube.com/watch?v=video_id_psy3" }
+         ]
+    },
+    "الفلسفة والمنطق": {
+        "مستر نادر جورج": [
+            { name: "الفلسفة البيئية", link: "https://www.youtube.com/watch?v=video_id_phil1" },
+            { name: "مراحل علاقة الإنسان بالبيئة", link: "https://www.youtube.com/watch?v=video_id_phil2" }
+        ]
+    },
+    "اللغة العربية": {
+        "مستر محمد صلاح": [
+            { name: "النحو الوحدة الأولى", link: "https://www.youtube.com/watch?v=video_id_4" }
+        ]
+    },
+    "اللغة الإنجليزية": {
+        "مستر عمرو رجب": [
+            { name: "Unit 1", link: "https://www.youtube.com/watch?v=video_id_eng1" }
+        ]
+    }
+};
