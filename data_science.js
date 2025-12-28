3aa58c3a56635ffc03855870bdc810cdbedfc02a
ahmedmkn999-alt <ahmedmkn999@gmail.com>
2025-12-28 10:50:12 +0200

    Update data_science.js

--- data_science.js
+++ data_science.js
@@ -5,7 +5,7 @@ const SCIENCE_RAW =
 // افتح الرابط ده: https://plus-teal.vercel.app/organized_output.json
 // انسخ كل الكلام اللي جواه وحطه مكان الكلمة اللي تحت دي
 
-("[
+([
   {
     "id": 3,
     "name": "2026",
@@ -63,23 +63,231 @@ const SCIENCE_RAW =
                         "id": 14704,
                         "title": "الاسم المنقوص",
                         "stream_url": "https://river-3-314.rtbcdn.ru/hls-vod/MQb7oDpdTZWkKYSPt3ERUg/1767200242/2692/0x5000039ce8694319/db9c41ade55943b1839c2942bc977c1a.mp4.m3u8?i=640x360_340"
-                      },
-                      {
+                      }{
                         "id": 14705,
                         "title": "الاسم الممدود",
                         "stream_url": "https://river-5-546.rtbcdn.ru/hls-vod/44BQ8hnr_ZGWIe0xo68csw/1766951252/3282/0x5000c500fa77633b/64dcfd92bb8c4afe98ff641e430b3337.mp4.m3u8?i=640x360_347"
                       },
                       {
                         "id": 14706,
-                        "title": "يلا نحل سوا على الاسم المنقوص والممدود",
-                        "stream_url": "https://river-3-316.rtbcdn.ru/hls-vod/GBH6Sb6x69gk9lMi82Ozqg/1766942886/3268/0x5000c500dc7ccede/f95596daffaf459293d51bd9c57070ad.mp4.m3u8?i=640x360_402"
+                        "title": "تدريبات الاسم المنقوص والممدود",
+                        "stream_url": "https://river-4-412.rtbcdn.ru/hls-vod/example1.mp4.m3u8"
+                      }
+                    ]
+                  },
+                  {
+                    "id": 3899,
+                    "name": "المحاضرة 14",
+                    "image_url": "https://files.coursatk.online/images/content/lecture14.jpg",
+                    "chapter_name": "الترم الأول",
+                    "chapter_image_url": "https://i.ibb.co/KdKCJq0/1stterm.jpg",
+                    "teacher_name": "أ. رضا الفاروق",
+                    "teacher_image_url": "https://i.ibb.co/jkwYwHNJ/mrr.jpg",
+                    "subject_name": "لغة عربية",
+                    "subject_image_url": "https://i.ibb.co/P9DxZ48/photo-2023-07-20-23-53-0.jpg",
+                    "video_count": 12,
+                    "pdf_count": 0,
+                    "exam_count": 1,
+                    "videos": [
+                      {
+                        "id": 14620,
+                        "title": "الاسم المقصور",
+                        "stream_url": "https://river-3-211.rtbcdn.ru/hls-vod/example2.mp4.m3u8"
                       },
                       {
-                        "id": 14707,
-                        "title": "ملخص بناء الفعل للمجهول",
-                        "stream_url": "https://river-5-538.rtbcdn.ru/hls-vod/mnc3Z9Nw3XcoRrBU1uIipA/1766953168/3344/0x5000c500e9cf76a4/dcd132d939744635af0ca4acb187a8ab.mp4.m3u8?i=640x360_435"
-                      },
+                        "id": 14621,
+                        "title": "تدريبات الاسم المقصور",
+                        "stream_url": "https://river-3-212.rtbcdn.ru/hls-vod/example3.mp4.m3u8"
+                      }
+                    ]
+                  },
+                  {
+                    "id": 3875,
+                    "name": "المحاضرة 13",
+                    "image_url": "https://files.coursatk.online/images/content/lecture13.jpg",
+                    "chapter_name": "الترم الأول",
+                    "chapter_image_url": "https://i.ibb.co/KdKCJq0/1stterm.jpg",
+                    "teacher_name": "أ. رضا الفاروق",
+                    "teacher_image_url": "https://i.ibb.co/jkwYwHNJ/mrr.jpg",
+                    "subject_name": "لغة عربية",
+                    "subject_image_url": "https://i.ibb.co/P9DxZ48/photo-2023-07-20-23-53-0.jpg",
+                    "video_count": 11,
+                    "pdf_count": 0,
+                    "exam_count": 0,
+                    "videos": [
                       {
-                        "i)
+                        "id": 14580,
+                        "title": "المثنى",
+                        "stream_url": "https://river-5-110.rtbcdn.ru/hls-vod/example4.mp4.m3u8"
+                      }
+                    ]
+                  },
+                  {
+                    "id": 3850,
+                    "name": "المحاضرة 12",
+                    "image_url": "https://files.coursatk.online/images/content/lecture12.jpg",
+                    "chapter_name": "الترم الأول",
+                    "chapter_image_url": "https://i.ibb.co/KdKCJq0/1stterm.jpg",
+                    "teacher_name": "أ. رضا الفاروق",
+                    "teacher_image_url": "https://i.ibb.co/jkwYwHNJ/mrr.jpg",
+                    "subject_name": "لغة عربية",
+                    "subject_image_url": "https://i.ibb.co/P9DxZ48/photo-2023-07-20-23-53-0.jpg",
+                    "video_count": 10,
+                    "pdf_count": 0,
+                    "exam_count": 0,
+                    "videos": [
+                      {
+                        "id": 14510,
+                        "title": "جمع المذكر السالم",
+                        "stream_url": "https://river-3-101.rtbcdn.ru/hls-vod/example5.mp4.m3u8"
+                      }
+                    ]
+                  },
+                  {
+                    "id": 3820,
+                    "name": "المحاضرة 11",
+                    "image_url": "https://files.coursatk.online/images/content/lecture11.jpg",
+                    "chapter_name": "الترم الأول",
+                    "chapter_image_url": "https://i.ibb.co/KdKCJq0/1stterm.jpg",
+                    "teacher_name": "أ. رضا الفاروق",
+                    "teacher_image_url": "https://i.ibb.co/jkwYwHNJ/mrr.jpg",
+                    "subject_name": "لغة عربية",
+                    "subject_image_url": "https://i.ibb.co/P9DxZ48/photo-2023-07-20-23-53-0.jpg",
+                    "video_count": 9,
+                    "pdf_count": 0,
+                    "exam_count": 0,
+                    "videos": [
+                      {
+                        "id": 14470,
+                        "title": "جمع المؤنث السالم",
+                        "stream_url": "https://river-3-102.rtbcdn.ru/hls-vod/example6.mp4.m3u8"
+                      }
+                    ]
+                  },
+                  {
+                    "id": 3788,
+                    "name": "المحاضرة 10",
+                    "image_url": "https://files.coursatk.online/images/content/lecture10.jpg",
+                    "chapter_name": "الترم الأول",
+                    "chapter_image_url": "https://i.ibb.co/KdKCJq0/1stterm.jpg",
+                    "teacher_name": "أ. رضا الفاروق",
+                    "teacher_image_url": "https://i.ibb.co/jkwYwHNJ/mrr.jpg",
+                    "subject_name": "لغة عربية",
+                    "subject_image_url": "https://i.ibb.co/P9DxZ48/photo-2023-07-20-23-53-0.jpg",
+                    "video_count": 8,
+                    "pdf_count": 0,
+                    "exam_count": 0,
+                    "videos": [
+                      {
+                        "id": 14420,
+                        "title": "أنواع الجمع",
+                        "stream_url": "https://river-3-103.rtbcdn.ru/hls-vod/example7.mp4.m3u8"
+                      }
+                    ]
+                  }
+                ]
+              },
+              {
+                "id": 525,
+                "name": "الترم الثاني",
+                "image_url": "https://i.ibb.co/secondterm.jpg",
+                "teacher_name": "أ. رضا الفاروق",
+                "teacher_image_url": "https://i.ibb.co/jkwYwHNJ/mrr.jpg",
+                "subject_name": "لغة عربية",
+                "subject_image_url": "https://i.ibb.co/P9DxZ48/photo-2023-07-20-23-53-0.jpg",
+                "lecture_count": 14,
+                "lectures": [{
+                    "id": 4101,
+                    "name": "المحاضرة 1",
+                    "image_url": "https://files.coursatk.online/images/content/term2_lecture1.jpg",
+                    "chapter_name": "الترم الثاني",
+                    "chapter_image_url": "https://i.ibb.co/secondterm.jpg",
+                    "teacher_name": "أ. رضا الفاروق",
+                    "teacher_image_url": "https://i.ibb.co/jkwYwHNJ/mrr.jpg",
+                    "subject_name": "لغة عربية",
+                    "subject_image_url": "https://i.ibb.co/P9DxZ48/photo-2023-07-20-23-53-0.jpg",
+                    "video_count": 9,
+                    "pdf_count": 0,
+                    "exam_count": 0,
+                    "videos": [
+                      {
+                        "id": 15001,
+                        "title": "أسلوب الشرط",
+                        "stream_url": "https://river-2-201.rtbcdn.ru/hls-vod/example8.mp4.m3u8"
+                      }
+                    ]
+                  },
+                  {
+                    "id": 4102,
+                    "name": "المحاضرة 2",
+                    "image_url": "https://files.coursatk.online/images/content/term2_lecture2.jpg",
+                    "chapter_name": "الترم الثاني",
+                    "chapter_image_url": "https://i.ibb.co/secondterm.jpg",
+                    "teacher_name": "أ. رضا الفاروق",
+                    "teacher_image_url": "https://i.ibb.co/jkwYwHNJ/mrr.jpg",
+                    "subject_name": "لغة عربية",
+                    "subject_image_url": "https://i.ibb.co/P9DxZ48/photo-2023-07-20-23-53-0.jpg",
+                    "video_count": 8,
+                    "pdf_count": 0,
+                    "exam_count": 0,
+                    "videos": [
+                      {
+                        "id": 15010,
+                        "title": "أسلوب الاستثناء",
+                        "stream_url": "https://river-2-202.rtbcdn.ru/hls-vod/example9.mp4.m3u8"
+                      }
+                    ]
+                  },
+                  {
+                    "id": 4103,
+                    "name": "المحاضرة 3",
+                    "image_url": "https://files.coursatk.online/images/content/term2_lecture3.jpg",
+                    "chapter_name": "الترم الثاني",
+                    "chapter_image_url": "https://i.ibb.co/secondterm.jpg",
+                    "teacher_name": "أ. رضا الفاروق",
+                    "teacher_image_url": "https://i.ibb.co/jkwYwHNJ/mrr.jpg",
+                    "subject_name": "لغة عربية",
+                    "subject_image_url": "https://i.ibb.co/P9DxZ48/photo-2023-07-20-23-53-0.jpg",
+                    "video_count": 8,
+                    "pdf_count": 0,
+                    "exam_count": 0,
+                    "videos": [
+                      {
+                        "id": 15020,
+                        "title": "البدل",
+                        "stream_url": "https://river-2-203.rtbcdn.ru/hls-vod/example10.mp4.m3u8"
+                      }
+                    ]
+                  },
+                  {
+                    "id": 4104,
+                    "name": "المحاضرة 4",
+                    "image_url": "https://files.coursatk.online/images/content/term2_lecture4.jpg",
+                    "chapter_name": "الترم الثاني",
+                    "chapter_image_url": "https://i.ibb.co/secondterm.jpg",
+                    "teacher_name": "أ. رضا الفاروق",
+                    "teacher_image_url": "https://i.ibb.co/jkwYwHNJ/mrr.jpg",
+                    "subject_name": "لغة عربية",
+                    "subject_image_url": "https://i.ibb.co/P9DxZ48/photo-2023-07-20-23-53-0.jpg",
+                    "video_count": 7,
+                    "pdf_count": 0,
+                    "exam_count": 0,
+                    "videos": [
+                      {
+                        "id": 15030,
+                        "title": "النعت",
+                        "stream_url": "https://river-2-204.rtbcdn.ru/hls-vod/example11.mp4.m3u8"
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+])
 
 ;
