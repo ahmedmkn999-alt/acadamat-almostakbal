34fc0355016dd9388213d3d18d3b8d444f52b54c
ahmedmkn999-alt <ahmedmkn999@gmail.com>
2025-12-28 11:15:22 +0200

    Update data_science.js

--- data_science.js
+++ data_science.js
@@ -24,22 +24,31 @@ const SCIENCE_RAW = [
                       { "title": "محتوى المحاضرة الـ 15", "stream_url": "https://river-3-344.rtbcdn.ru/hls-vod/HgPMvgvaabK5kW5uFdYKpA/1767200235/3320/0x5000c500e9ee263c/831db01913874978980d98501b896df8.mp4.m3u8?i=640x360_457" },
                       { "title": "الاسم المنقوص", "stream_url": "https://river-3-314.rtbcdn.ru/hls-vod/MQb7oDpdTZWkKYSPt3ERUg/1767200242/2692/0x5000039ce8694319/db9c41ade55943b1839c2942bc977c1a.mp4.m3u8?i=640x360_340" },
                       { "title": "الاسم الممدود", "stream_url": "https://river-5-546.rtbcdn.ru/hls-vod/44BQ8hnr_ZGWIe0xo68csw/1766951252/3282/0x5000c500fa77633b/64dcfd92bb8c4afe98ff641e430b3337.mp4.m3u8?i=640x360_347" },
-                      { "title": "حل على الاسم المنقوص والممدود", "stream_url": "https://river-3-316.rtbcdn.ru/hls-vod/GBH6Sb6x69gk9lMi82Ozqg/1766942886/3268/0x5000c500dc7ccede/f95596daffaf459293d51bd9c57070ad.mp4.m3u8?i=640x360_402" },
+                      { "title": "تدريبات (منقوص وممدود)", "stream_url": "https://river-3-316.rtbcdn.ru/hls-vod/GBH6Sb6x69gk9lMi82Ozqg/1766942886/3268/0x5000c500dc7ccede/f95596daffaf459293d51bd9c57070ad.mp4.m3u8?i=640x360_402" },
                       { "title": "بناء الفعل للمجهول", "stream_url": "https://river-5-538.rtbcdn.ru/hls-vod/mnc3Z9Nw3XcoRrBU1uIipA/1766953168/3344/0x5000c500e9cf76a4/dcd132d939744635af0ca4acb187a8ab.mp4.m3u8?i=640x360_435" },
-                      { "title": "تأنيث الفعل وجوبًا وجوازًا", "stream_url": "https://river-3-370.rtbcdn.ru/hls-vod/m-2dsvT9_sD1vwXX6TWJoQ/1766954135/3348/0x5000c500fa803aeb/3e0b0f945e6c48939b0f67ce052fa315.mp4.m3u8?i=640x360_354" },
-                      { "title": "أدب الديوان (الجزء الثاني)", "stream_url": "https://river-3-359.rtbcdn.ru/hls-vod/wrOxEA859P592J526SjE2A/1767012063/3366/0x5000c500fa78bb1c/ccdeaa866c294c928e2cb3da7e0d7ac4.mp4.m3u8?i=640x360_366" },
-                      { "title": "الفصل الأخير من الجزء الأول قصة", "stream_url": "https://river-5-512.rtbcdn.ru/hls-vod/MzPzwSfsNJZBh4i8NhMeyA/1767200300/3346/0x5000c500e9cf3135/58ee139d359e4f789d006bf602b24ca0.mp4.m3u8?i=640x360_433" }
+                      { "title": "تأنيث الفعل", "stream_url": "https://river-3-370.rtbcdn.ru/hls-vod/m-2dsvT9_sD1vwXX6TWJoQ/1766954135/3348/0x5000c500fa803aeb/3e0b0f945e6c48939b0f67ce052fa315.mp4.m3u8?i=640x360_354" },
+                      { "title": "حل أسئلة الوحدة الثانية", "stream_url": "https://river-3-355.rtbcdn.ru/hls-vod/VRcVdyZFSJRHjCW2YY_lZQ/1766954375/3316/0x5000c500e9eb5839/ce785b3ed0224a77b874524f9a0e06bf.mp4.m3u8?i=640x360_381" },
+                      { "title": "أدب الديوان (الجزء 2)", "stream_url": "https://river-3-359.rtbcdn.ru/hls-vod/wrOxEA859P592J526SjE2A/1767012063/3366/0x5000c500fa78bb1c/ccdeaa866c294c928e2cb3da7e0d7ac4.mp4.m3u8?i=640x360_366" },
+                      { "title": "تدريبات أدب الديوان", "stream_url": "https://river-4-416.rtbcdn.ru/hls-vod/9m7BcyJpBOpYKa1UPaAdLg/1766990088/3308/0x5000c500e9cf27a0/20f05d66e44848c1a83b97d4f32388b6.mp4.m3u8?i=640x360_401" },
+                      { "title": "نص متحرر 1 (أدب الديوان)", "stream_url": "https://river-3-343.rtbcdn.ru/hls-vod/oJv35CLxGhwezW1Y7VQ5yg/1767018230/3360/0x5000c500fa6e9fe5/443c8d9a37b24d0bac91447ba8969527.mp4.m3u8?i=640x360_424" },
+                      { "title": "نص متحرر 2 (أدب الديوان)", "stream_url": "https://river-3-370.rtbcdn.ru/hls-vod/w4ePLxA6hYDBMJMWSMyNoQ/1767019815/3330/0x5000c500e9cf59b9/9a7a1d82e3814dc9bda0800fc6576b60.mp4.m3u8?i=640x360_367" },
+                      { "title": "القصة (نهاية الجزء الأول)", "stream_url": "https://river-5-512.rtbcdn.ru/hls-vod/MzPzwSfsNJZBh4i8NhMeyA/1767200300/3346/0x5000c500e9cf3135/58ee139d359e4f789d006bf602b24ca0.mp4.m3u8?i=640x360_433" },
+                      { "title": "تدريبات القصة", "stream_url": "https://river-3-362.rtbcdn.ru/hls-vod/aRmFyJwqg1FFyJz2k2881A/1767119745/1859/0x5000c500e53682e2/3fa9f9a8bc244bf981ffb74a82743784.mp4.m3u8?i=640x360_404" }
                     ]
                   },
                   {
                     "id": 3896,
                     "name": "المحاضرة 14 (الاسم المقصور)",
                     "videos": [
-                      { "title": "محتوى المحاضرة الـ 14", "stream_url": "https://river-3-347.rtbcdn.ru/hls-vod/0AL1lDOq0MY6XltlkxOugA/1767119522/3278/0x5000c500fa70080b/423ff8261f04435ca62ff0b89b58a7b9.mp4.m3u8?i=640x360_333" },
+                      { "title": "محتوى المحاضرة 14", "stream_url": "https://river-3-347.rtbcdn.ru/hls-vod/0AL1lDOq0MY6XltlkxOugA/1767119522/3278/0x5000c500fa70080b/423ff8261f04435ca62ff0b89b58a7b9.mp4.m3u8?i=640x360_333" },
                       { "title": "الاسم المقصور", "stream_url": "https://river-5-557.rtbcdn.ru/hls-vod/rDSlOkDWEkHpb-7dKKgtyA/1767118957/3298/0x5000c500e9ebbe4c/cd10a7167f64460b93c095e91eae3662.mp4.m3u8?i=640x360_310" },
                       { "title": "اسم المرة واسم الهيئة", "stream_url": "https://river-4-424.rtbcdn.ru/hls-vod/FPBleBFP6w_bKXEMIxEU6w/1766943310/3298/0x5000c500e9cc3df1/5e592326e7ac4c43aba442b05ed58c7f.mp4.m3u8?i=640x360_295" },
-                      { "title": "أدب مدرسة الديوان (الجزء الأول)", "stream_url": "https://river-3-368.rtbcdn.ru/hls-vod/QUQqvy7yHuaHwBuSsC_5iA/1767120663/3282/0x5000c500fa77b760/fa12b77ffa7a49bda6e42d7b3016888d.mp4.m3u8?i=640x360_328" },
-                      { "title": "الفصل العاشر من قصة الأيام", "stream_url": "https://river-3-374.rtbcdn.ru/hls-vod/XpHHzkCOkLIwCjBxJWiuyw/1766998226/3306/0x5000c50074476884/7374aab8405f4e63a53f3e23a7791d4f.mp4.m3u8?i=640x360_333" }
+                      { "title": "تدريبات نحو", "stream_url": "https://river-4-421.rtbcdn.ru/hls-vod/jHlAy9HVA-9Z2QMXSY9lfg/1767200344/3318/0x5000c500e927ac67/6e029594a4c04b34b6f3c814c408fe58.mp4.m3u8?i=640x360_333" },
+                      { "title": "أدب مدرسة الديوان (جزء 1)", "stream_url": "https://river-3-368.rtbcdn.ru/hls-vod/QUQqvy7yHuaHwBuSsC_5iA/1767120663/3282/0x5000c500fa77b760/fa12b77ffa7a49bda6e42d7b3016888d.mp4.m3u8?i=640x360_328" },
+                      { "title": "ورشة نصوص متحررة", "stream_url": "https://river-5-512.rtbcdn.ru/hls-vod/f8Va4PFjvEEJSz3zQLD2VA/1767200355/2734/0x5000039ce8638101/31ef51286b09438fb88b99f96c1f3969.mp4.m3u8?i=640x360_364" },
+                      { "title": "القصة (الفصل العاشر)", "stream_url": "https://river-3-374.rtbcdn.ru/hls-vod/XpHHzkCOkLIwCjBxJWiuyw/1766998226/3306/0x5000c50074476884/7374aab8405f4e63a53f3e23a7791d4f.mp4.m3u8?i=640x360_333" },
+                      { "title": "تدريبات القصة", "stream_url": "https://river-4-411.rtbcdn.ru/hls-vod/HJoMMGaD364y1NsXSc36lQ/1767011459/3340/0x5000c500e9cf2094/3f7e7d42a7064254bd6af6f9af1a718b.mp4.m3u8?i=640x360_382" },
+                      { "title": "حل واجب المحاضرة 14", "stream_url": "https://river-3-349.rtbcdn.ru/hls-vod/wNQxtzXH87zIS2zqpIlEOQ/1767120057/3340/0x5000c500e986fb9d/82d8ee9ad2464ff3b62c6dfadbd4e414.mp4.m3u8?i=640x360_284" }
                     ]
                   },
                   {
@@ -49,7 +58,13 @@ const SCIENCE_RAW = [
                       { "title": "المصادر الخماسية", "stream_url": "https://river-3-336.rtbcdn.ru/hls-vod/A-fu0EUqeI4K5lHurDef4Q/1767200396/3404/0x5000c500dbbca3c9/9d145c0fbb60487598b412c3956609b6.mp4.m3u8?i=640x360_323" },
                       { "title": "المصدر المؤول", "stream_url": "https://river-3-345.rtbcdn.ru/hls-vod/yDncm6OmFgPqQfDuISDmeA/1767094334/2644/0x5000039ce838d655/b2f0de92a86640df860714daf5d356c5.mp4.m3u8?i=640x360_338" },
                       { "title": "البناء للمجهول", "stream_url": "https://river-4-421.rtbcdn.ru/hls-vod/ql1jVH8GovqEjap6JZjZuQ/1767053532/3346/0x5000c500e9cc14cc/8405e17791f04c9891861956f3589ea5.mp4.m3u8?i=640x360_375" },
-                      { "title": "بلاغة - الإيجاز", "stream_url": "https://river-5-548.rtbcdn.ru/hls-vod/0LMHNeq8jTf338wC6WKZfg/1767063993/2664/0x5000039ce832fc71/4266bf24774c4f47a1b8c42607686abc.mp4.m3u8?i=640x360_316" }
+                      { "title": "تدريبات نحو", "stream_url": "https://river-4-403.rtbcdn.ru/hls-vod/wdjj7RidgI_lrYwv4BrDnA/1767200416/3346/0x5000c500e9cc14cc/bfeb1c1294644d16af814ac1854cd09d.mp4.m3u8?i=640x360_351" },
+                      { "title": "بلاغة - الإيجاز", "stream_url": "https://river-5-548.rtbcdn.ru/hls-vod/0LMHNeq8jTf338wC6WKZfg/1767063993/2664/0x5000039ce832fc71/4266bf24774c4f47a1b8c42607686abc.mp4.m3u8?i=640x360_316" },
+                      { "title": "تدريبات بلاغة", "stream_url": "https://river-3-311.rtbcdn.ru/hls-vod/EW2GUDffwWnGEp6nBuh8xw/1767200428/3350/0x5000c500e9ebb76a/d939c7ddfd074138b84d93285afffad1.mp4.m3u8?i=640x360_374" },
+                      { "title": "ورشة نصوص متحررة", "stream_url": "https://river-3-336.rtbcdn.ru/hls-vod/xrf5dDocwLvc_taEmjq9hg/1767200434/3286/0x5000c500fa7838da/aea82f7d904b4100a217267d35ada010.mp4.m3u8?i=640x360_384" },
+                      { "title": "القصة", "stream_url": "https://river-3-363.rtbcdn.ru/hls-vod/vKLBB6pGFQrM96FbCMam0A/1767200440/3256/0x5000c500e9ebfadb/c36cc8171b604448b8fd7072a27a19c8.mp4.m3u8?i=640x360_380" },
+                      { "title": "تدريبات القصة", "stream_url": "https://river-3-311.rtbcdn.ru/hls-vod/yNpo6S88oT8946C-HVNz-w/1767200446/3286/0x5000c500fa776343/217647a82d5f4b5cbea28e1402cfee92.mp4.m3u8?i=640x360_403" },
+                      { "title": "حل واجب المحاضرة 13", "stream_url": "https://river-3-340.rtbcdn.ru/hls-vod/f2sAcwIKYgfgnuIRg6iTvA/1767200452/2700/0x5000039ce86904e1/f7f366caf24e490e86f495088f27dbfb.mp4.m3u8?i=640x360_279" }
                     ]
                   },
                   {
@@ -57,17 +72,69 @@ const SCIENCE_RAW = [
                     "name": "المحاضرة 10 (اسم التفضيل)",
                     "videos": [
                       { "title": "صياغة اسم التفضيل", "stream_url": "https://river-3-363.rtbcdn.ru/hls-vod/UGwUgWGOB0gJ9LIqbZErJw/1767141528/3330/0x5000c500e9d36bcc/f37b30dee7d3453eb8297b2411fe5929.mp4.m3u8?i=640x360_296" },
+                      { "title": "صور وحالات اسم التفضيل", "stream_url": "https://river-5-534.rtbcdn.ru/hls-vod/JX3hCZyocRh61l_AnGrkgA/1766948364/2203/0x5000c5007446e2a2/d0e9a6eeaa554334a35dcc4165f200d6.mp4.m3u8?i=640x360_284" },
                       { "title": "أسلوب المدح والذم", "stream_url": "https://river-3-314.rtbcdn.ru/hls-vod/N4sVbR_QZST0Z-nVC04Jlw/1767200649/3282/0x5000c500fa78b70f/bdb8f12f9cee472d9c59d8b70fa7659b.mp4.m3u8?i=640x360_293" },
-                      { "title": "الخيال المركب", "stream_url": "https://river-3-346.rtbcdn.ru/hls-vod/Q74QBNoTN-SaCVOozM1B9w/1767115169/3266/0x5000c500e9d37e6b/06996f99328244c69536840a853b2a36.mp4.m3u8?i=640x360_318" }
+                      { "title": "الخيال المركب", "stream_url": "https://river-3-346.rtbcdn.ru/hls-vod/Q74QBNoTN-SaCVOozM1B9w/1767115169/3266/0x5000c500e9d37e6b/06996f99328244c69536840a853b2a36.mp4.m3u8?i=640x360_318" },
+                      { "title": "الخيال الممتد", "stream_url": "https://river-3-314.rtbcdn.ru/hls-vod/XU6UbpdJHz0HF8uG4bclBQ/1767034010/3358/0x5000c500fa7007d9/e77728c0859a4d2c88229b365cb290a6.mp4.m3u8?i=640x360_316" },
+                      { "title": "حل واجب المحاضرة 10", "stream_url": "https://river-4-424.rtbcdn.ru/hls-vod/3IFyPHxYp-Oya5ifwa8ggw/1767040156/3350/0x5000c500e9ebfe30/a83ffcf34223449ea590e3b6b0fd6f22.mp4.m3u8?i=640x360_263" }
                     ]
                   },
                   {
                     "id": 12059,
                     "name": "كورس المكثف (التلاتة بتلاتين)",
                     "videos": [
-                      { "title": "اليوم الأول بالتفصيل", "stream_url": "https://river-3-355.rtbcdn.ru/hls-vod/QjqqFkf1mxl-3IUggbBpig/1767098227/3378/0x5000c500fa6e7d9a/6d21495ebc524c0c883861e5c49921c9.mp4.m3u8?i=640x360_726" },
-                      { "title": "اليوم الثاني بالتفصيل", "stream_url": "https://river-3-329.rtbcdn.ru/hls-vod/amA2Xqrmt5vjq3EPI1VWPw/1767092668/3342/0x5000c500e9d0660a/3f7f91eeba6040ebaf3bd2f534e9aad9.mp4.m3u8?i=640x360_803" },
-                      { "title": "اليوم الثالث بالتفصيل", "stream_url": "https://river-3-357.rtbcdn.ru/hls-vod/MCYEyn0g6NH7fVbnyzy5DA/1767098395/2706/0x5000039ce86363a9/f2575cdfb49b445ba4fc1b1d922f03ef.mp4.m3u8?i=640x360_546" }
+                      { "title": "اليوم الأول (تفصيل + حل)", "stream_url": "https://river-3-355.rtbcdn.ru/hls-vod/QjqqFkf1mxl-3IUggbBpig/1767098227/3378/0x5000c500fa6e7d9a/6d21495ebc524c0c883861e5c49921c9.mp4.m3u8?i=640x360_726" },
+                      { "title": "خلاصة اليوم الأول", "stream_url": "https://river-5-513.rtbcdn.ru/hls-vod/8MON9QENqPpH08Cj6uhCNg/1767158718/3260/0x5000c500e9bd898c/9a312718e1c440f8a633cd814f4a54dd.mp4.m3u8?i=640x360_836" },
+                      { "title": "اليوم الثاني (تفصيل + حل)", "stream_url": "https://river-3-329.rtbcdn.ru/hls-vod/amA2Xqrmt5vjq3EPI1VWPw/1767092668/3342/0x5000c500e9d0660a/3f7f91eeba6040ebaf3bd2f534e9aad9.mp4.m3u8?i=640x360_803" },
+                      { "title": "خلاصة اليوم الثاني", "stream_url": "https://river-5-549.rtbcdn.ru/hls-vod/WU87uSBehmnAwkVtpFqBuw/1767200986/3322/0x5000c500e9ebfce3/eec68695ee4245999615569a74266b63.mp4.m3u8?i=640x360_878" },
+                      { "title": "اليوم الثالث (تفصيل + حل)", "stream_url": "https://river-3-357.rtbcdn.ru/hls-vod/MCYEyn0g6NH7fVbnyzy5DA/1767098395/2706/0x5000039ce86363a9/f2575cdfb49b445ba4fc1b1d922f03ef.mp4.m3u8?i=640x360_546" },
+                      { "title": "خلاصة اليوم الثالث", "stream_url": "https://river-5-534.rtbcdn.ru/hls-vod/yPR3Uwqg282GpIuV9uGHlQ/1767208612/3350/0x5000c500e9ebfad5/9b2b92b00daa4355bc54ce3d78f92793.mp4.m3u8?i=640x360_539" }
+                    ]
+                  },
+                  {
+                    "id": 12046,
+                    "name": "المحاضرة 5 (لا النافية للجنس)",
+                    "videos": [
+                      { "title": "شروط اسم لا النافية للجنس", "stream_url": "https://river-3-371.rtbcdn.ru/hls-vod/BWqd13tj1QVoxsEnU7c6ZA/1767177035/2654/0x5000039ce868f9bd/21529866e7d444dabbedf7f6b9e51577.mp4.m3u8?i=640x360_793" },
+                      { "title": "أنواع اسم لا", "stream_url": "https://river-3-361.rtbcdn.ru/hls-vod/p01V2rULWFZtLj7LTaLBfQ/1767178812/3358/0x5000c500fa774e8d/39240acc08da4d38826df3857a686153.mp4.m3u8?i=640x360_745" },
+                      { "title": "تدريبات نحو", "stream_url": "https://river-5-517.rtbcdn.ru/hls-vod/SAzOVqdpkFm8SFBq_uaOYg/1766961161/1887/0x5000c500e53c0380/9d07c3c3fb02469396077bdb54c3902c.mp4.m3u8?i=640x360_785" },
+                      { "title": "الكناية والمجاز", "stream_url": "https://river-3-371.rtbcdn.ru/hls-vod/8_xN9VEK6tzFWErqUIVhEw/1767201061/3292/0x5000c500fa775024/42d2bd1d450c46498ebf4b8deab61718.mp4.m3u8?i=640x360_859" },
+                      { "title": "القصة (الفصل 2)", "stream_url": "https://river-3-354.rtbcdn.ru/hls-vod/liZ7AmKuGrZOWI5vYKg9uA/1767201084/3354/0x5000c500e986c71c/87bacfe23ddb444aa8aa02378ba6f219.mp4.m3u8?i=640x360_822" },
+                      { "title": "القصة (الفصل 3)", "stream_url": "https://river-3-358.rtbcdn.ru/hls-vod/HJJHlVcTH-KnL-KCKDQ7og/1767201090/3356/0x5000c500fa77d18d/12f365b0ddd24cc9b19bd131144bb001.mp4.m3u8?i=640x360_890" },
+                      { "title": "حل واجب المحاضرة 5", "stream_url": "https://river-3-352.rtbcdn.ru/hls-vod/2xXFsnUANlh12kEdWzLUtA/1767201101/3318/0x5000c500e8e8b6bb/92298679e67c40fe995712d1bd25d31d.mp4.m3u8?i=640x360_442" }
+                    ]
+                  },
+                  {
+                    "id": 12036,
+                    "name": "المحاضرة 4 (إن وأخواتها)",
+                    "videos": [
+                      { "title": "إن وأخواتها", "stream_url": "https://river-4-402.rtbcdn.ru/hls-vod/Ix5gG1HfOkf_oryb2GPY0g/1766985687/3350/0x5000c500e9ebf399/9e7de062349445c089ffd7102a1904fa.mp4.m3u8?i=640x360_691" },
+                      { "title": "الضمائر", "stream_url": "https://river-5-551.rtbcdn.ru/hls-vod/zTiRax_0-p4jCDr5XOhIdA/1767019868/3274/0x5000c500e9d3737a/54184916df1743eb99c1c0c3e56da616.mp4.m3u8?i=640x360_760" },
+                      { "title": "أدب الإحياء والبعث (جزء 2)", "stream_url": "https://river-3-355.rtbcdn.ru/hls-vod/13xi6svmTArGYIR6AETdmQ/1767201134/3338/0x5000c500e9ebb93d/ee037969ba7740ee8376deb866b82698.mp4.m3u8?i=640x360_819" },
+                      { "title": "تدريبات أدب", "stream_url": "https://river-5-537.rtbcdn.ru/hls-vod/l9GkPICKCgIh0-zqIHJYJA/1766953682/3342/0x5000c500e9ed3d87/3698695c61834764ac497442ae3c208c.mp4.m3u8?i=640x360_712" },
+                      { "title": "كيف تحل نصوص متحررة", "stream_url": "https://river-3-374.rtbcdn.ru/hls-vod/91t8nhfOp80sfykIWiYbug/1766998876/3256/0x5000c500e9cfb617/9c325f4afc994687a2816e0a5a27827a.mp4.m3u8?i=640x360_751" },
+                      { "title": "حل واجب المحاضرة 4", "stream_url": "https://river-4-416.rtbcdn.ru/hls-vod/wA70a7m0iCfhfYuuaReeiA/1767190846/3260/0x5000c500e9cfb812/88a369e5f9204258a7deb12cce8c36bb.mp4.m3u8?i=640x360_664" }
+                    ]
+                  },
+                  {
+                    "id": 12025,
+                    "name": "المحاضرة 3 (كان وأخواتها)",
+                    "videos": [
+                      { "title": "كان وأخواتها", "stream_url": "https://river-5-547.rtbcdn.ru/hls-vod/JglJv8VL292feFQ4a2qZwQ/1767049719/3264/0x5000c500e9ebfc23/70b701943047496f8c9eb17dd4ba1908.mp4.m3u8?i=640x360_698" },
+                      { "title": "مستويات عليا (كان وأخواتها)", "stream_url": "https://river-3-341.rtbcdn.ru/hls-vod/_ipwQbeDUiBZngBr2Coxpg/1767053679/3302/0x5000c500e9ebdebf/65665326062a4a4fb2a4788ad3a813fb.mp4.m3u8?i=640x360_643" },
+                      { "title": "كان التامة", "stream_url": "https://river-5-514.rtbcdn.ru/hls-vod/mYfkf2UQ4G8tF5SJRdf8XA/1767111542/2219/0x5000c5007446e2a8/6c34323af749472d88f3488a8b6e4cec.mp4.m3u8?i=640x360_617" },
+                      { "title": "كاد وأخواتها", "stream_url": "https://river-3-359.rtbcdn.ru/hls-vod/bAnjLl1rzi2K2KZaCL9lkQ/1767199341/3256/0x5000c500e9ebfadb/9a9df868c3224e8d927f4b2b9729ea29.mp4.m3u8?i=640x360_675" },
+                      { "title": "القصة (الفصل الأول)", "stream_url": "https://river-5-513.rtbcdn.ru/hls-vod/LZ79U01f--kxb9V0hqi0Kg/1767201221/3322/0x5000c500e9cfaffc/5493b8b34c144e8797fa21a445809f71.mp4.m3u8?i=640x360_787" },
+                      { "title": "حل واجب المحاضرة 3", "stream_url": "https://river-3-346.rtbcdn.ru/hls-vod/k5uqqoCBw3aYlXCApwIxUw/1767201234/3376/0x5000c500fa77602a/93e6199db1d54c6d933bc70b8c488c1c.mp4.m3u8?i=640x360_701" }
+                    ]
+                  },
+                  {
+                    "id": 12014,
+                    "name": "المحاضرة 2 (تقديم الخبر)",
+                    "videos": [
+                      { "title": "تقديم الخبر وجوبًا", "stream_url": "https://river-3-343.rtbcdn.ru/hls-vod/x4oC53Q-5iFnVgtdRqcq3g/1766945271/3402/0x5000c500fa776335/6f5eaee5dad543dbbc7d75e967aa95f1.mp4.m3u8?i=640x360_652" },
+                      { "title": "تقديم الخبر جوازًا", "stream_url": "https://river-5-541.rtbcdn.ru/hls-vod/jcixylciaxe87UK9TVmALA/1766949426/3374/0x5000c500fa774f3b/f25c108730464808b79b76655b1eb47b.mp4.m3u8?i=640x360_689" },
+                      { "title": "حذف الخبر وجوبًا", "stream_url": "https://river-3-347.rtbcdn.ru/hls-vod/KTOVDjuR7gd6mS0OY" } // (هذا الرابط كان ناقص في المصدر فأكملته للإغلاق)
                     ]
                   }
                 ]
